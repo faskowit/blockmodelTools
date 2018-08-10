3348ccbef7695826d47bf0e26f3565f7abcc9a12
faskowit <jfaskowi@iu.edu>
2018-08-10 10:58:32 -0400

    add to synth mat func

--- src/tools/wsbm_synth_adj_gen.m
+++ src/tools/wsbm_synth_adj_gen.m
@@ -1,8 +1,12 @@
-function [genMat,noNaGenMat] = wsbm_synth_adj_gen(wsbmModel)
+function [genMat,noNaGenMat] = wsbm_synth_adj_gen(wsbmModel,sym)
 % generate a synthetic mat given the parameters of a already realized wsbm
 % model fit
 %
 % function [Edge_List,True_Model] = generateEdges(W_truth,E_truth,R,theta_w,theta_e,group_Size,degree_Para)
+
+if nargin < 2
+    sym = 1 ;
+end 
 
 weightDist_model = wsbmModel.W_Distr ;
 edgeDist_model = wsbmModel.E_Distr ;
@@ -27,7 +31,9 @@ edgeList = generateEdges(...
 genMat = Edg2Adj(edgeList) ;
 
 % symmetrize the genMat
-genMat = triu(genMat) + triu(genMat,1)';
+if sym == 1
+    genMat = triu(genMat) + triu(genMat,1)';
+end
 
 if nargout > 1
     noNaGenMat =  genMat ;
