d4924aa1a1943b6ea6c20475f6baac0cd796a753
faskowit <jfaskowi@iu.edu>
2019-01-22 15:21:58 -0500

    not really sure about this reordering...

--- src/visualization/wsbm_reorder_block_mod.m
+++ src/visualization/wsbm_reorder_block_mod.m
@@ -1,8 +1,7 @@
 function [newOrderInd] = wsbm_reorder_block_mod(iMat,ca)
 
 % get average block matrix
-[~,avgblMat] = get_block_mat(iMat,ca) ;
+blockMat = get_block_mat(iMat,ca) ;
 
 % reorder_mod on the block matrix
-newOrderInd = reorder_mod(avgblMat,unique(ca)) ;
-%newOrderInd = flipud(newOrderInd) ;
+newOrderInd = reorder_mod(blockMat,unique(ca)) ;
